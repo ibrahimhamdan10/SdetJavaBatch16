5af6666ec8cbb91a335b2f750106ff2e829cf561
ibrahimhamdan10 <ibrahimkhaledhamdan10@gmail.com>
2023-04-17 02:22:58 -0400

    hala

--- src/ProjectHW/Task6.java
+++ src/ProjectHW/Task6.java
@@ -10,14 +10,15 @@ public class Task6 {
         boolean isPrime=true;
         for (int i = 2; i <num/2 ; i++) {
             if(num%i==0){
-                isPrime=false;
-                System.out.println("IS not prime");
+                isPrime=true;
                 break;
             }
         }
-            if(isPrime){
-                System.out.println(num+" is Prime");
+        if (isPrime)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
 
         }
     }
-}
+
---
+++ src/training/repilt3_2Darray.java
@@ -0,0 +1,127 @@
+package training;
+
+import java.util.Arrays;
+
+public class repilt3_2Darray {
+    public static void main(String[] args) {
+        /*
+        double [][]numbers={
+                {1.4,2.0,3.3,2.0},
+                {4.0,1.5,6.1,1.0},
+                {1.2,3.1,4.0,1.6}
+        };
+        for (int i = 0; i <numbers.length ; i++) {
+            for (int j = 0; j <numbers[i].length ; j++) {
+                System.out.print(numbers[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+         */
+        /*
+        double[][] a = {
+                {1.4,2.0,3.3,2},
+                {4,1.5,6.1,1},
+                {1.2,3.1,4,1.6}
+        };
+
+        for(int i =0; i< a.length;i++){
+            for ( int j = 0; j <a[i].length ; j++) {
+              a[i][j]*=2;  //very good step
+                System.out.print(a[i][j]+" ");
+                }
+            System.out.println();
+            }
+
+         */
+        /*
+            int [] arr1D={5,4,8};
+           int highest=arr1D[0];
+
+        for (int i = 0; i <arr1D.length ; i++) {
+            if(arr1D[i]>highest){
+                highest=arr1D[i];
+            }
+        }
+        System.out.println(highest);
+
+         */
+        /*
+
+        int[][] a = {
+                {-5,-2,-3,7},
+                {1,-5,-2, 2},
+                {1,-2, 3,-4}
+        };
+
+        int sum=0;
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[i].length;j++){
+                sum+=a[i][j];
+            }
+        }
+        System.out.println(sum);
+
+         */
+        /*
+
+
+        int[][] a = {
+                {1,1,2},
+                {3,1,2},
+                {3,5,3},
+                {0,1,2}
+        };
+
+        for (int i = 0; i <a.length ; i++) {
+            int sum=0;
+            for (int j = 0; j <a[i].length ; j++) {
+                sum += a[i][j];
+            }
+            System.out.println(sum);
+        }
+
+         */
+        /*
+
+        int[][] a = {
+                {-5,-2,-3,7},
+                {1,-5,-2,2},
+                {1,-2,3,-4}
+        };
+        int counter=0;
+        for (int i = 0; i <a.length ; i++) {
+            for (int j = 0; j <a[i].length ; j++) {
+                if(a[i][j]%2!=0&&a[i][j]<0){
+                    counter++;
+                }
+            }
+
+        } System.out.println(counter);
+
+         */
+
+        int[][] a = {
+                {-5,-2,-3,7},
+                {1,-5,-2,2},
+                {1,-2,3,-4}
+        };
+        int sum=0;
+        for (int i = 0; i <a.length ; i+=2) {
+            for (int j = 0; j <a[i].length ; j+=2) {
+                    sum+=a[i][j];
+            }
+        } System.out.println(sum);
+
+
+
+
+
+    }
+
+    }
+
+
+
+
+
